15420eb1f104b04d5e8c0a674c2fcb8a7c5cce17
Albuquerque <filippialbuquerque@gmail.com>
2023-09-20 12:43:28 -0300

    random quote

--- FrontEnd Final Projects/Random Quote Machine/random-quote/src/App.js
+++ FrontEnd Final Projects/Random Quote Machine/random-quote/src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
+
 
 
 function App() {
   return (
     <div>
       <Header name="Ana" color="purple"/>
+      <Main />
     </div>
   )
 }
 
-export default App;
 
+
+
+export default App;
+ //dfg
+ 
 
 
 
---
+++ FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+    return <p>"There are no traffic jams along the extra mile, from main.js" </p>
+}
+
+export default Main;
