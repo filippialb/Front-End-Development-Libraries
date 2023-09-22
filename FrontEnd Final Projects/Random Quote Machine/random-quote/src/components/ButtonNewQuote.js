f4343c01a700176f2a3eeccaa1d522e4006c5940
Albuquerque <filippialbuquerque@gmail.com>
2023-09-22 13:33:05 -0300

    random quote

--- FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/ButtonNewQuote.js
+++ FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/ButtonNewQuote.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 
+
+const quotes = [
+
+["Life isn’t about getting and having, it’s about giving and being.","Kevin Kruse"],
+["Whatever the mind of man can conceive and believe, it can achieve.","Napoleon Hill"],
+["Strive not to be a success, but rather to be of value.","Albert Einstein"],
+["Two roads diverged in a wood, and I—I took the one less traveled by.","Robert Frost"],
+]
+
+
+
+
+
 function ButtonNewQuote() {
   // State to store the generated random number
   const [randomNumber, setRandomNumber] = useState(null);
 
   // Function to generate and set a random number
   const generateRandomNumber = () => {
-    const newRandomNumber = Math.floor(Math.random() * 100); // Change the range as needed
+    const newRandomNumber = quotes[Math.floor(Math.random() * quotes.length)]; // Change the range as needed
     setRandomNumber(newRandomNumber);
   };
 
