d57097a8b98a73a1ff8b77aa4bf929d7c9bbe76c
Albuquerque <filippialbuquerque@gmail.com>
2023-09-26 13:13:44 -0300

    random quote

--- FrontEnd Final Projects/Random Quote Machine/random-quote/src/App.js
+++ FrontEnd Final Projects/Random Quote Machine/random-quote/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import ButtonNewQuote from './components/ButtonNewQuote';
 import React from 'react';
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
 
 
 function App() {
--- FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/ButtonNewQuote.js
+++ FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/ButtonNewQuote.js
@@ -1,44 +1,47 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+
+class ButtonNewQuoteClass extends Component {
+  state = {
+    quotes: [
+      ["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
+      ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
+      ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+      ["Two roads diverged in a wood, and I—I took the one less traveled by.", "Robert Frost"],
+    ],
+    randomNumber: null,
+  };
+
+  generateRandomNumber = () => {
+    const { quotes } = this.state;
+    const newRandomNumber = quotes[Math.floor(Math.random() * quotes.length)];
+    this.setState({ randomNumber: newRandomNumber });
 
 
-const quotes = [
+   document.getElementById("body").style.backgroundColor = 'rgba('
+        + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) 
+        + ',' + Math.floor(Math.random() * 255) + '\)'   
+  };
 
-["Life isn’t about getting and having, it’s about giving and being.","Kevin Kruse"],
-["Whatever the mind of man can conceive and believe, it can achieve.","Napoleon Hill"],
-["Strive not to be a success, but rather to be of value.","Albert Einstein"],
-["Two roads diverged in a wood, and I—I took the one less traveled by.","Robert Frost"],
-]
+  componentDidMount() {
+    this.generateRandomNumber();
+  }
 
-
-    function ButtonNewQuote() {
-    // State to store the generated random number
-    const [randomNumber, setRandomNumber] = useState(null);
-
-    // Function to generate and set a random number
-    const generateRandomNumber = () => {
-        var newRandomNumber = quotes[Math.floor(Math.random() * quotes.length)]; // Change the range as needed
-        setRandomNumber(newRandomNumber);
-
-        
-    };
-
-    //--
-
-
-
-
-
+  render() {
+    const { randomNumber } = this.state;
 
     return (
-        <div>
-        
+      <div>
         {randomNumber !== null && (
-            <p>Random Number: {randomNumber}</p>
+          <div>
+            <p>Random Quote:</p>
+            <p>{randomNumber[0]}</p>
+            <p>- {randomNumber[1]}</p>
+          </div>
         )}
-        <button onClick={generateRandomNumber}>New Quote</button>
-        </div>
+        <button onClick={this.generateRandomNumber}>New Quote</button>
+      </div>
     );
-    }
+  }
+}
 
-
-export default ButtonNewQuote;
+export default ButtonNewQuoteClass;
