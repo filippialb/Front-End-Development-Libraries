d4adc854a7d90bd9b23f8dc14a039a05d9142347
Albuquerque <filippialbuquerque@gmail.com>
2023-10-22 14:35:55 -0300

    ecommerce

--- FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Header(props) {
-    return <h1>Hello there, {props.name}, {props.color} </h1>
-}
-
-export default Header;
--- FrontEnd Final Projects/Random Quote Machine/random-quote/src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Main() {
-    return true
-}
-
-
-export default Main;
- 
